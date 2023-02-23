9715f9fc0357da553f44fdf7082bd9b0e2c06f35
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-23 12:44:33 -0500

    using caporal

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
 const program = require("caporal");
 
-const start = debounce(() => {
-  console.log("starting users program");
-}, 100);
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action((args) => {
+    console.log(args);
+  });
+program.parse(process.argv);
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => console.log("FILE CHANGED"))
-  .on("unlink", () => console.log("FILE UNLINKED"));
+// const start = debounce(() => {
+//   console.log("starting users program");
+// }, 100);
+
+// chokidar
+//   .watch(".")
+//   .on("add", start)
+//   .on("change", () => console.log("FILE CHANGED"))
+//   .on("unlink", () => console.log("FILE UNLINKED"));
